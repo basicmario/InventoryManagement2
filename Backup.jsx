3a148ee3e96167520abe411afe8d498387f6fd08
unknown <romariot2004@gmail.com>
2026-02-27 16:32:57 -0500

    Initial commit

---
+++ Backup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Backup() {
+  return (
+    <>
+      <div className="backup">
+        <div className="header">
+          <p>Back up and recovery</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Backup
---
+++ src/Usermanagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Usermanagement() {
+  return (
+    <div>Usermanagement</div>
+  )
+}
+
+export default Usermanagement
