ccee38e4ead0b9afb60e664c7c068e8139572727
unknown <romariot2004@gmail.com>
2026-03-02 16:55:18 -0500

    First commit

--- src/Inventorycontrol.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Inventorycontrol() {
-  return (
-    <div>Inventorycontrol</div>
-  )
-}
-
-export default Inventorycontrol
--- src/Usermanagement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Usermanagement() {
-  return (
-    <div>Usermanagement</div>
-  )
-}
-
-export default Usermanagement
